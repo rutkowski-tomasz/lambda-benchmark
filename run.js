dded773687f295f3e340bd100628b457e2a53ab4
Tomasz Rutkowski <rutkowski.tomasz.3@gmail.com>
2025-10-20 20:32:00 +0200

    feat: custom image building

--- run.js
+++ run.js
@@ -28,10 +28,15 @@ const memorySizes = [
     128
 ];
 
-const packageSizes = {};
-await packAll(runtimes, architectures);
-fs.writeFileSync('data/packages.json', JSON.stringify(packageSizes, null, 2));
-await executeAll(runtimes, architectures, memorySizes);
+const ACCOUNT_ID = "024853653660";
+const REGION = "eu-central-1";
+
+// const packageSizes = {};
+// await packAll(runtimes, architectures);
+// fs.writeFileSync('data/packages.json', JSON.stringify(packageSizes, null, 2));
+// await executeAll(runtimes, architectures, memorySizes);
+
+await createCustomImage('dotnet8', 'arm64');
 
 export async function packAll(runtimes, architectures) {
 
@@ -73,6 +78,30 @@ export async function pack(runtime, architecture) {
     const size = fs.statSync(packPath).size;
     packageSizes[`${runtime}-${architecture}`] = size;
     console.log(`[success] Packed ${runtime} - ${architecture} (${packPath}, ${formatSize(size)})`);
+}
+
+export async function createCustomImage(runtime, architecture) {
+    const configFile = fs.readFileSync(`runtimes/${runtime}/config.json`, 'utf8');
+    const config = JSON.parse(configFile);
+    const platform = architecture === 'arm64' ? 'linux/arm64' : 'linux/amd64';
+    const extractPath = `runtimes/${runtime}/function_${architecture}`;
+
+    if (fs.existsSync(extractPath)) {
+        fs.rmSync(extractPath, { recursive: true, force: true });
+    }
+    
+    await execAsync(`unzip runtimes/${runtime}/function_${architecture}.zip -d ${extractPath}`);
+
+    const tag = `${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com/lambda-benchmark:${runtime}-${architecture}`;
+    const buildCommand = `docker build --platform ${platform} runtimes/${runtime} -f Dockerfile.custom_image --build-arg BASE_IMAGE=${config.baseImage} --build-arg HANDLER=${config.handler} --build-arg ARCHITECTURE=${architecture} -t ${tag}`;
+
+    console.log('buildCommand', buildCommand);
+
+    await execAsync(buildCommand);
+
+    await execAsync(`aws ecr get-login-password --region ${REGION} | docker login --username AWS --password-stdin ${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com`);
+
+    await execAsync(`docker push ${tag}`);
 }
 
 export async function executeAll(runtimes, architectures, memorySizes) {
